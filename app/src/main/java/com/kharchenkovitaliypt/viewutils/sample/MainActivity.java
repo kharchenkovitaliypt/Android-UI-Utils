416625539ba85afa98c3dc0535cf6cd376909184
Vitaliy Kharchenko <kharchenkovitaliypt@gmail.com>
2017-03-05 12:22:50 +0200

    Init

---
+++ app/src/main/java/com/kharchenkovitaliypt/viewutils/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kharchenkovitaliypt.viewutils.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.kharchenkovitaliypt.viewutils.R.layout.activity_main);
+    }
+}
